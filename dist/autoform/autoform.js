7fad7497c668cd43149191737284dae2e189388b
origin1tech <origin1tech@gmail.com>
2014-12-15 19:25:46 -0800

    bug fixes

---
+++ dist/autoform/autoform.js
@@ -0,0 +1,340 @@
+angular.module('ai.autoform', [])
+
+.provider('$autoform', function $autoform() {
+
+    var defaults = {
+
+            prefix: 'model',           // value to prepend to models.
+            labels: true,              // when true labels are created.
+            type: 'text',              // the default type for elements.
+            addClass: false,           // specify class to be added to form for styling.
+            placeholders: true,        // when true placeholders are created for text elements.
+            textareaAt: 35,            // if value is greater than this use textarea.
+
+            datetimeExp:               // date time expression
+                /^(0?[1-9]|1[012])(:[0-5]\d) [APap][mM]$/,
+
+            phoneExp:                   // phone expression
+                /^(?!.*911.*\d{4})((\+?1[\/ ]?)?(?![\(\. -]?555.*)\( ?[2-9][0-9]{2} ?\) ?|(\+?1[\.\/ -])?[2-9][0-9]{2}[\.\/ -]?)(?!555.?01..)([2-9][0-9]{2})[\.\/ -]?([0-9]{4})$/,
+
+            intExp:                     // integer/number expression.
+                /^\d+$/,
+                                        // email expression.
+            emailExp:
+                /^[\w-]+(\.[\w-]+)*@([a-z0-9-]+(\.[a-z0-9-]+)*?\.[a-z]{2,6}|(\d{1,3}\.){3}\d{1,3})(:\d{4})?$/
+
+        },
+        get, set;
+
+    set = function (options) {
+        defaults = angular.extend(defaults, options);
+    };
+
+    get = ['$rootScope', '$compile', function ($rootScope, $compile) {
+
+        // iterate attributes build string.
+        function parseAttributes(attrs) {
+            if(!attrs || !angular.isObject(attrs)) return '';
+            var result = [];
+            angular.forEach(attrs, function (v, k) {
+                if(k === 'ngModel')
+                    k = 'ng-model';
+                result.push(k + '="' + v + '"');
+            });
+            return result.join(' ');
+        }
+
+        // tests if value is boolean.
+        function isBoolean(value) {
+            if(typeof value === 'boolean')
+                return true;
+            return !!(value == 'true' || value == 'false');
+        }
+
+        // tests if array contains value.
+        function contains(arr, value){
+            return arr.indexOf(value) !== -1;
+        }
+
+        // generate tabs.
+        function tab(qty) {
+            var _tab = '',
+                ctr = 0;
+            while(ctr < qty){
+                _tab += '\t';
+                ctr += 1;
+            }
+            return _tab;
+        }
+
+        // trim string.
+        function trim(str) {
+            return str.replace(/^\s+/, '').replace(/\s+$/, '');
+        }
+
+        function capitalize(str) {
+            return str.charAt(0).toUpperCase() + str.slice(1);
+        }
+
+        // parse the element type.
+        function parseType(value, options) {
+            if(isBoolean(value))
+                return 'checkbox';
+            if(options.phoneExp.test(value))
+                return 'tel';
+            if(options.datetimeExp.test(value))
+                return 'datetime';
+            if(options.intExp.test(value))
+                return 'number';
+            if(options.emailExp.test(value))
+                return 'email';
+            return options.type;
+        }
+
+        function generateRadios(values, attrs){
+            var radAttrs = attrs,
+                radios = '';
+            angular.forEach(values, function (v) {
+                var radOpts = '\t\t<label class="checkbox-inline"><input {{ATTRS}}/>' +
+                    ' {{NAME}}</label>\n\t</div>';
+                radios += (tab(3) + radOpts
+                    .replace('{{ATTRS}}', trim(radAttrs + ' value="' + v + '"')))
+                    .replace('{{NAME}}', v);
+            });
+            return radios;
+        }
+
+        function ModuleFactory(element, options) {
+
+            var $module = {},
+                template = '',
+                scope,
+                elements;
+
+            options = options || {};
+            $module.scope = scope = options.scope || $rootScope.$new();
+            $module.options = scope.options = options = angular.extend(angular.copy(defaults), options);
+            elements = options.elements || {};
+
+            // initialize the module.
+            function init() {
+
+                var template = '',
+                    groups = [];
+
+                // normalize the data.
+                angular.forEach(options.source, function (v,k) {
+
+                    // parse source get type & attributes
+                    var elem = elements[k] = elements[k] || {},
+                        capName = capitalize(k),
+                        attrs,
+                        tmpValue,
+                        el;
+                    attrs = elem.attributes = elem.attributes || {};
+
+                    // set type
+                    var origType = attrs.type || elem.type;
+                    attrs.type = origType;
+
+                    // normalize name attribute.
+                    attrs.name = attrs.name || k;
+
+                    // set value.
+                    attrs.value = v || attrs.value || elem.value;
+                    attrs.type = parseType(attrs.value, options);
+
+                    // set default class value.
+                    attrs.class = attrs.class || '';
+
+                    // if ngModel is false delete from attrs.
+                    // if undefined create adding prefix.
+                    if(attrs.ngModel === false || elem.ngModel === false)
+                        delete attrs.ngModel;
+                    else
+                        attrs.ngModel =
+                            attrs.ngModel ||
+                            elem.ngModel ||
+                            options.prefix ? options.prefix + '.' + k : k;
+
+                    // if values array/object
+                    // supplied set type as select
+                    // if not specified by user
+                    // and not of type radio.
+                    if(elem.values){
+                        if(angular.isString(elem.values))
+                            elem.values = elem.values.split(',');
+                        if(!angular.isArray(elem.values) && angular.isObject(elem.values)){
+                            // if is object can only be select.
+                            attrs.type = 'select';
+                        }
+                        if(angular.isArray(elem.values)){
+                            attrs.type = attrs.type === 'radio' ? 'radio' : 'select';
+                        }
+                    }
+
+                    // test if textarea is needed.
+                    if(attrs.value && angular.isString(attrs.value) && attrs.value.length > options.textareaAt)
+                        attrs.type = 'textarea';
+
+                    // add default class for inputs/selects.
+                    if(!contains(['textarea', 'checkbox', 'radio'], attrs.type))
+                        attrs.class = trim(attrs.class.replace('form-control', '') + ' form-control');
+
+                    // store value temporarily.
+                    tmpValue = attrs.value;
+
+                    // set value as checked/selected if radio
+                    // or select delete from attrs.
+                    if(contains(['select', 'radio', 'textarea'], attrs.type)){
+                        if(attrs.type === 'radio')
+                            elem.checked = attrs.value;
+                        if(attrs.type === 'select')
+                            elem.selected = attrs.value;
+                        if(attrs.type === 'textarea')
+                            elem.content = attrs.value;
+                        delete attrs.value;
+                    }
+
+                    // parse all attributes.
+                    var attrsStr = elem.attributesStr = parseAttributes(attrs);
+
+                    // add value back in after parsing.
+                    attrs.value = tmpValue;
+
+                    var group = '';
+
+                    // create opening group markup.
+                    if(attrs.type !== 'checkbox')
+                        group += (tab(1) + '<div class="form-group">');
+                    else
+                        group += (tab(1) + '<div class="checkbox">');
+
+                    // generate labels.
+                    if(attrs.type !== 'radio') {
+                        var label = (tab(2) + '<label{{FOR}}>');
+                        if(attrs.type !== 'checkbox')
+                            label = label.replace('{{FOR}}', ' for="' + k + '"') + capName + '</label>';
+                        else   
+                            label = label.replace('{{FOR}}', ' class="checkbox"');
+
+                        // labels are required for checkboxes.
+                        if(options.labels || attrs.type === 'checkbox')
+                            group += label;
+                    }
+
+                    // check if non-standard input type.
+                    if(contains(['select', 'textarea', 'radio'], attrs.type)) {
+
+                        if(attrs.type === 'textarea'){
+                            el = '<textarea {{ATTRS}}>{{CONTENT}}</textarea>';
+                            el = el.replace('{{CONTENT}}', attrs.value);
+                        }
+
+                        if(attrs.type === 'select'){
+                            var opts = '',
+                                isKeyVal = !angular.isArray(elem.values);
+                            el = '<select {{ATTRS}}>{{OPTIONS}}</select>';
+                            angular.forEach(elem.values, function (v,k) {
+                                var opt = (tab(4) + '<option {{VALUE}}>{{TEXT}}</option>');
+                                if(isKeyVal)
+                                    opt = (opt.replace('{{VALUE}}', k).replace('{{TEXT}}', v));
+                                else
+                                    opt = (opt.replace('{{VALUE}}', '').replace('{{TEXT}}', v));
+                                opts += opt;
+                            });
+                            el = el.replace('{{OPTIONS}}', opts);
+                        }
+
+                        if(attrs.type === 'radio'){
+
+                            group += generateRadios(elem.values, attrsStr);
+
+                        }
+
+                    } else {
+                        el = '<input {{ATTRS}}/>';
+                    }
+
+                    // add attribute string.
+                    if(el) {
+                        el = (tab(3) + el.replace('{{ATTRS}}', attrsStr));
+                        // add element to group.
+                        group += el;
+                    }
+
+                    // close label if radio or checkbox.
+                    if(attrs.type === 'checkbox')
+                        group += (tab(2) + capName + '</label>');
+                    
+                    // close markup group.
+                    //if(!contains(['radio', 'checkbox'], attrs.type))
+                        group += (tab(1) + '</div>');
+                    
+                    // add to the collection.
+                    groups.push(group);
+
+                });
+
+                var wrapper = angular.element('<div></div>'),
+                    form = angular.element('<form></form>');
+
+                template = groups.join('\n');
+                element.replaceWith(wrapper);
+                form.html(template);
+                wrapper.append(form);
+
+                scope.model = options.source;
+                $compile(wrapper.contents())(scope);
+
+                return $module;
+
+            }
+
+            return init();
+
+        }
+
+        return ModuleFactory;
+
+    }];
+
+    return {
+        $get: get,
+        $set: set
+    };
+
+})
+
+.directive('aiAutoform', ['$autoform', function($autoform) {
+
+    return {
+        restrict: 'AC',
+        scope: true,
+        //priority: -1,
+        link: function (scope, element, attrs) {
+
+            var defaults, options, $module;
+
+            defaults = {
+                scope: scope
+            };
+
+            function init() {
+                // create the directive.
+                $module = $autoform(element, options);
+            }
+
+            // get options and model.
+            options = angular.extend(defaults, scope.$eval(attrs.aiAutoform || attrs.options));
+
+            // define the source.
+            options.source = options.source || scope.$eval(attrs.source);
+
+            init();
+
+        }
+
+    };
+
+}]);
---
+++ dist/common/app.js
@@ -0,0 +1,323 @@
+/**
+ * NOTE this file is only used for examples
+ * and documentation and is not part of
+ * the Ai library.
+ */
+
+define(function (require) {
+
+    require('step/step.js');
+    require('table/table.js');
+    require('widget/widget.js');
+    require('flash/flash.js');
+    require('modal/modal.js');
+    require('validate/validate.js');
+    require('viewer/viewer.js');
+    require('storage/storage.js');
+    require('passport/passport.js');
+    require('dropdown/dropdown.js');
+    require('autoform/autoform.js');
+
+
+    var app, tmpAreas, keys, areas, controller;
+
+    tmpAreas = {
+        '/register': 'Angular application registration helper',
+        '/passport': 'Simplifies handling authentication within Angular app.',
+        '/storage': 'Use local storage with auto cookie fallback.',
+        '/autoform': 'Handy during dev to quickly create a form.',
+        '/dropdown': 'Advanced customizable dropdown directive.',
+        '/flash': 'Allows for showing flash/popup messages.',
+        '/modal': 'Native modal dialog.',
+        '/table': 'Bind local or remote data to table/grid.',
+        '/validate': 'Form validation with auto model binding.',
+        '/viewer': 'Animated slide transitions for views.',
+        '/widget': 'Various widgets - case, nicescroll, redactor, decimal, input mask & compare.',
+        '/step': 'Creates form step wizard.'
+    };
+
+    // sort areas.
+    keys = Object.keys(tmpAreas).sort();
+    areas = {};
+
+    angular.forEach(keys, function (k) {
+        areas[k] = tmpAreas[k];
+    });
+
+    app = angular.module('app', ['ngRoute', 'ngAnimate', 'ai.step', 'ai.table', 'ai.storage', 'ai.dropdown',
+        'ai.widget', 'ai.modal', 'ai.flash', 'ai.viewer', 'ai.passport', 'ai.validate', 'ai.autoform']);
+
+    app.config(['$routeProvider', '$locationProvider', '$passportProvider',
+        function ($routeProvider, $locationProvider, $passportProvider) {
+            $routeProvider.when('/', {templateUrl: '/home.html', controller: 'Controller', title: 'Ai'});
+            angular.forEach(areas, function (v, k) {
+                var config, cap;
+                cap = k.replace('/', '');
+                cap = cap.charAt(0).toUpperCase() + cap.slice(1);
+                config = {
+                    templateUrl: '/' + cap.toLowerCase() + '/example' + k + '.html',
+                    controller: 'Controller', title: 'Ai ' + cap
+                };
+                $routeProvider.when(k, config);
+            });
+            $locationProvider.html5Mode(true);
+
+        }]);
+
+    app.run(['$rootScope', function ($rootScope) {
+        $rootScope.msg = '';
+        $rootScope.close = function () {
+            $rootScope.msg = '';
+        };
+        $rootScope.showBack = function () {
+            return $rootScope.title !== 'Ai';
+        };
+        $rootScope.$on('$routeChangeSuccess', function (event, current) {
+            $rootScope.msg = '';
+            $rootScope.title = current.$$route.title;
+        });
+    }]);
+
+    controller = [
+        '$rootScope', '$scope', '$route', '$flash', '$step', '$modal', '$storage',
+        function ($rootScope, $scope, $route, $flash, $step, $modal, $storage) {
+            var current = $route.current,
+                route = current.$$route,
+                params = current.params,
+                path = route.originalPath,
+                area = path && path !== '/' ? path.replace('/', '') : 'home';
+
+            $scope.tabs = {
+                table:      { active: 'markup' },
+                step:       { active: 'markup' },
+                flash:      { active: 'markup' },
+                decimal:    { active: 'markup' },
+                casing:     { active: 'markup' },
+                compare:    { active: 'markup' },
+                modal:      { active: 'markup' },
+                storage:    { active: 'markup' },
+                viewer:     { active: 'markup' },
+                dropdown:   { active: 'markup' },
+                autoform:   { active: 'markup' },
+                placeholder:{ active: 'markup' }
+            };
+
+            $scope.tabActive = function (key) {
+                if(!angular.isString(key) || key.indexOf(':') === -1)
+                    return;
+                key = key.split(':');
+                return $scope.tabs[key[0]].active === key[1];
+            };
+
+            $scope.showTab = function (key) {
+                if(!angular.isString(key) || key.indexOf(':') === -1)
+                    return;
+                key = key.split(':');
+                $scope.tabs[key[0]].active = key[1];
+            };
+
+            if (area === 'home') {
+                $scope.items = areas;
+            }
+
+            if (area === 'table') {
+                $scope.tbl = {
+                    source: [
+                        {name: 'Jim Thomas', email: 'jthomas@gmail.com'},
+                        {name: 'Bob Blair', email: 'bblair@aol.com'},
+                        {name: 'Randy Quick', email: 'randy.quick@yahoo.com'},
+                        {name: 'Bob Smith', email: 'bobsmith@msn.com'},
+                        {name: 'Susan Jones', email: 'susan.jones@hotmail.com'},
+                        {name: 'Larry Anderson', email: 'anderson.larry@mail.com'},
+                        {name: 'Harry Ellis', email: 'ellis@gmail.com'},
+                        {name: 'Rory Boscoe', email: 'rory@aol.com'},
+                        {name: 'Simon Green', email: 'sgreen@yahoo.com'},
+                        {name: 'Quentin Rose', email: 'qrose@msn.com'},
+                        {name: 'Micah Barry', email: 'barry4u@hotmail.com'},
+                        {name: 'Alex Angle', email: 'alex.angle@mail.com'}
+                    ],
+                    orderBy: ['-name'],
+                    selectable: true,
+                    exportable: true,
+                    display: 10,
+                    hover: true,
+                    goto: false
+                };
+            }
+
+            if (area === 'validate') {
+                $scope.phonePattern = /^(?!.*911.*\d{4})((\+?1[\/ ]?)?(?![\(\. -]?555.*)\( ?[2-9][0-9]{2} ?\) ?|(\+?1[\.\/ -])?[2-9][0-9]{2}[\.\/ -]?)(?!555.?01..)([2-9][0-9]{2})[\.\/ -]?([0-9]{4})$/;
+                $scope.submitValidate = function (form, model) {
+                    if (form.$invalid) return;
+                    // alert feedback.
+                    alert('Passed validation your data would submit here.');
+                };
+                $scope.validateFormConf = {
+                    validators: {
+                        phone: {
+                            'ng-pattern': function (obj, form) {
+                                // eval the value to get orig
+                                // source expression used in ng-pattern.
+                                var getExp = $scope.$eval(obj.value);
+                                return '{{name}} does not match the pattern required.';
+                            }
+                        }
+                    }
+                };
+            }
+
+            if (area === 'widget') {
+                $scope.decimal = 25;
+                $scope.case = 'timothy';
+
+            }
+
+            if (area === 'flash') {
+                var flash = $flash;
+                $scope.flashMessage = 'Show Me!';
+                $scope.flashType = 'info';
+                $scope.flashTimeout = 0;
+                $scope.showFlash = function (msg, type, to) {
+                    flash.add(msg, type, to);
+                };
+            }
+
+            if (area === 'step') {
+
+                $scope.user = {
+                    username: 'bob@test.com',
+                    password: 'temptemp',
+                    first: 'Bob',
+                    last: 'Jones'
+                };
+                $scope.stepOptions = {
+                    steps: {
+                        credentials: {content: '/step/example/step-cred.html'},
+                        name: {content: '/step/example/step-name.html'},
+                        finish: {content: '/step/example/step-finish.html'}
+                    },
+                    onLoad: function ($module) {
+                        $module.steps[1].enabled = false;
+                    }
+                };
+
+
+            }
+
+            if (area === 'modal'){
+                var modalConf = {
+                    title: 'Instance Modal',
+                    content: 'I was shown using instance from source code.'
+                };
+                var modal = new $modal(modalConf);
+                $scope.showModal = function () {
+                    modal.show();
+                };
+            }
+
+            if (area === 'storage'){
+                var storage = $storage();
+                storage.set('storage', '');
+                $scope.setStorage = function (v) {
+                    storage.set('storage', v);
+                };
+                $scope.getStorage = function () {
+                    $scope.viewStorage = storage.get('storage');
+                };
+            }
+
+            if (area === 'dropdown'){
+
+                // basic dropdown example.
+                $scope.ddSimple = {
+                    text: 'name',
+                    value: 'email',
+                    inline: true,
+                    source: [
+                        {name: 'Jim Evers', email: 'jim@global.net', category: 'customer' },
+                        {name: 'Charles Xander', email: 'charles@gmail.com', category: 'customer'},
+                        {name: 'Scott Sandres', email: 'sanders.scott@aol.com', category: 'customer'},
+                        {name: 'Rob Reiner', email: 'rr@dc.rr.com', category: 'customer'},
+                        {name: 'Jim Thomas', email: 'jthomas@gmail.com', category: 'customer'},
+                        {name: 'Bob Blair', email: 'bblair@aol.com', category: 'employee'},
+                        {name: 'Randy Quick', email: 'randy.quick@yahoo.com', category: 'employee'},
+                        {name: 'Bob Smith', email: 'bobsmith@msn.com', category: 'employee'},
+                        {name: 'Susan Jones', email: 'susan.jones@hotmail.com', category: 'employee'},
+                        {name: 'Larry Anderson', email: 'anderson.larry@mail.com', category: 'employee'},
+                        {name: 'Harry Ellis', email: 'ellis@gmail.com', category: 'employee'},
+                        {name: 'Rory Boscoe', email: 'rory@aol.com', category: 'vendor'},
+                        {name: 'Simon Green', email: 'sgreen@yahoo.com', category: 'vendor'},
+                        {name: 'Quentin Rose', email: 'qrose@msn.com', category: 'vendor'},
+                        {name: 'Micah Barry', email: 'barry4u@hotmail.com', category: 'vendor'},
+                        {name: 'Alex Angle', email: 'alex.angle@mail.com', category: 'vendor'}
+                    ]
+                };
+                $scope.ddSimpleSel = 'charles@gmail.com';
+
+                // advanced example using grouping.
+                $scope.ddAdv = {
+                    text: 'name',
+                    value: 'email',
+                    groupKey: 'category',
+                    source: [
+                        {name: 'Jim Evers', email: 'jim@global.net', category: 'customer' },
+                        {name: 'Charles Xander', email: 'charles@gmail.com', category: 'customer'},
+                        {name: 'Scott Sandres', email: 'sanders.scott@aol.com', category: 'customer'},
+                        {name: 'Rob Reiner', email: 'rr@dc.rr.com', category: 'customer'},
+                        {name: 'Jim Thomas', email: 'jthomas@gmail.com', category: 'customer'},
+                        {name: 'Bob Blair', email: 'bblair@aol.com', category: 'employee'},
+                        {name: 'Randy Quick', email: 'randy.quick@yahoo.com', category: 'employee'},
+                        {name: 'Bob Smith', email: 'bobsmith@msn.com', category: 'employee'},
+                        {name: 'Susan Jones', email: 'susan.jones@hotmail.com', category: 'employee'},
+                        {name: 'Larry Anderson', email: 'anderson.larry@mail.com', category: 'employee'},
+                        {name: 'Harry Ellis', email: 'ellis@gmail.com', category: 'employee'},
+                        {name: 'Rory Boscoe', email: 'rory@aol.com', category: 'vendor'},
+                        {name: 'Simon Green', email: 'sgreen@yahoo.com', category: 'vendor'},
+                        {name: 'Quentin Rose', email: 'qrose@msn.com', category: 'vendor'},
+                        {name: 'Micah Barry', email: 'barry4u@hotmail.com', category: 'vendor'},
+                        {name: 'Alex Angle', email: 'alex.angle@mail.com', category: 'vendor'}
+                    ]
+                };
+                $scope.ddAdvSel = 'ellis@gmail.com';
+
+                // example using remote url.
+                $scope.ddRemote = {
+                    text: 'name',
+                    value: 'email',
+                    source: '/dropdown/example/data.json'
+                };
+
+            }
+
+            if (area === 'autoform') {
+                $scope.autoSource = {
+                    name: 'Nancy Brewer',
+                    email: 'nancy@gmail.com',
+                    phone: '8885551212',
+                    age: 23,
+                    enabled: true
+                };
+                $scope.autoOptions = {
+                    elements: {
+                        email: { type: 'email' },
+                        age: { type: 'radio', values: [19,20,21,22,23,24,25] }
+                    }
+                };
+            }
+        }
+    ];
+
+    app.controller('Controller', controller);
+
+    app.filter('areaName', function () {
+        return function (val) {
+            val = val.replace('/', '');
+            return val.charAt(0).toUpperCase() + val.slice(1);
+        };
+    });
+
+    angular.element(document).ready(function () {
+        angular.bootstrap(document, ['app']);
+    });
+
+});
